4e8d0c945e44fb3970c88e9ccfc569cb7285e9cb
Johanna Mayer (inf266) <johanna.mayer@smail.th-koeln.de>
2018-05-30 14:26:13 +0200

    Moving Pic

--- ForumPlanLogin/app/src/main/java/com/example/jojo/forumplanlogin/PlanActivity.java
+++ ForumPlanLogin/app/src/main/java/com/example/jojo/forumplanlogin/PlanActivity.java
@@ -5,14 +5,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class PlanActivity extends AppCompatActivity implements OnClickListener{
+public class PlanActivity extends AppCompatActivity implements OnClickListener {
 
 
     Button btnEGOG;
+    ImageView ivEG;
+
+    ViewGroup mainLayout;
+
+    private int xDelta;
+    private int yDelta;
 
 
     @Override
@@ -25,8 +35,48 @@ public class PlanActivity extends AppCompatActivity implements OnClickListener{
         btnEGOG = findViewById(R.id.btnEGOG);
         btnEGOG.setOnClickListener(this);
 
+        mainLayout = (ViewGroup) findViewById(R.id.mainLayout);
+        ivEG = mainLayout.findViewById(R.id.ivEG);
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(800, 1600);
+        ivEG.setLayoutParams(layoutParams);
+        ivEG.setOnTouchListener(new ChoiceTouchListener());
+
+
         }
 
+
+        private final class ChoiceTouchListener implements View.OnTouchListener{
+
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch ( event.getAction() & MotionEvent.ACTION_MASK){
+
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) v.getLayoutParams();
+                        xDelta = x- lParams.leftMargin;
+                        yDelta = y - lParams.topMargin;
+                        break;
+
+
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) v.getLayoutParams();
+                        layoutParams.leftMargin = x- xDelta;
+                        layoutParams.topMargin = y-yDelta;
+                        layoutParams.rightMargin = -2500;
+                        layoutParams.bottomMargin = -2500;
+                        v.setLayoutParams(layoutParams);
+                        break;
+
+                }
+                mainLayout.invalidate();
+                return true;
+            }
+        }
     @Override
     public void onClick(View v) {
 
@@ -38,5 +88,6 @@ public class PlanActivity extends AppCompatActivity implements OnClickListener{
         }
 
     }
+
 
 }
