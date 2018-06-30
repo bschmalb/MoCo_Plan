f3964799e13db8c292fd9a8a4c25347e0dc8fc75
Johanna Mayer (inf266) <johanna.mayer@smail.th-koeln.de>
2018-06-30 15:17:11 +0200

    Logo, App name

--- ShoppingMap/app/src/main/java/io/moxd/team9/shoppingMap/StartActivity.java
+++ ShoppingMap/app/src/main/java/io/moxd/team9/shoppingMap/StartActivity.java
@@ -15,6 +15,7 @@ public class StartActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT= 2000;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
